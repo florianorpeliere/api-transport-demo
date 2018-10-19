f25b0a2beae34ad00726d119d9abd1ab3eb1a6fb
Christophe Virot <christophe.virot@gmail.com>
2018-10-19 23:39:15 +0200

    Implement /api/lines with an example calling xml api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
+const cityCode = '217';
+const host = 'http://timeo3.keolis.com/relais/' + cityCode + '.php';
 
 router.get('/', function(req, res) {
     res.json('Welcome to the API !');
 });
 
 router.get('/lines', function(req, res) {
-    // FIXME : implement this method
-    res.json('bus / tram lines');
+    axios.get(host + '?xml=1')
+        .then(responses => {
+            // FIXME : Convert XML data to json format
+            res.json(responses.data);
+        })
+        .catch(err => {
+            console.warn(err);
+            res.status(500).json({error: 'Internal error'});
+        });
 });
 
 router.get('/stops', function(req, res) {
@@ -15,9 +26,9 @@ router.get('/stops', function(req, res) {
     res.json('bus / tram stops');
 });
 
-router.get('/schedules', function(req, res) {
+router.get('/stops/:reference/schedules', function(req, res) {
     // FIXME : implement this method
-    res.json('bus / tram stop schedules');
+    res.json('bus / tram stop schedules ' + req.params.reference);
 });
 
 module.exports = router;
